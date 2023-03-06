e208df9e246552be6eb2e07c7b6bd3c44685a44c
RubenJamartPXL <ruben.jamart@student.pxl.be>
2023-03-06 15:04:27 +0100

    dsqq

--- Chapter7/Exercise13/MainWindow.xaml.cs
+++ Chapter7/Exercise13/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace Exercise13
 {
@@ -7,6 +9,20 @@ namespace Exercise13
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void berekenButton_Click(object sender, RoutedEventArgs e)
+        {
+            double netto = Convert.ToDouble(priceTextbox.Text);
+            double tarief = 0.21;
+            if ((bool)tariefCheckBox.IsChecked)
+            {
+                tarief = 0.06;
+            }
+            double btw = netto * tarief;
+            double totaal = netto * (1 + tarief);
+            btwTextbox.Text = btw.ToString();
+            totalTextbox.Text = totaal.ToString();
         }
     }
 }
--- Chapter7/Exercise14/MainWindow.xaml.cs
+++ Chapter7/Exercise14/MainWindow.xaml.cs
@@ -8,5 +8,34 @@ namespace Exercise14
         {
             InitializeComponent();
         }
+
+        private void berekenButton_Click(object sender, RoutedEventArgs e)
+        {
+            string boodschap = "";
+            string geslacht = "";
+            string leeftijd = "";
+
+            //naam + voornaam (textbox)
+            boodschap += lastNameTextBox.Text + " " + firstNameTextBox.Text;
+
+            //geslacht (radiobutton)
+            if (MaleRadioButton.IsChecked == true)
+                geslacht = " man ";
+            else if (FemaleRadioButton.IsChecked == true)
+                geslacht = " vrouw ";
+            boodschap += geslacht;
+
+            //leeftijd (radiobutton)
+            if (leeftijd1Button.IsChecked == true)
+                leeftijd = "tussen 18 en 30 jaar";
+            else if (leeftijd2Button.IsChecked == true)
+                leeftijd = "tussen 30 en 45 jaar";
+            else if (leeftijd3Button.IsChecked == true)
+                leeftijd = "tussen 45 en 60 jaar";
+            else if (leeftijd4Button.IsChecked == true)
+                leeftijd = "ouder dan 60";
+            boodschap += leeftijd;
+            MessageBox.Show(boodschap);
+        }
     }
 }
